99b8989ffb6f371468600083159b85515699dd49
surba778 <surba778@gmail.com>
2022-03-14 17:25:55 +0000

    updated context processor

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
